a635bdccbb05e9dde5b0f92e24e1a44cebf24749
abdlkaderkhatir <a.khatir@esi-sba.dz>
2024-01-16 19:04:45 +0100

    innit commit

--- server.js
+++ server.js
@@ -1,64 +1,15 @@
-// core module in node.js
-var http = require('http');
-// core module in node.js
-var fs = require('fs');
-// core module in node.js
-var path = require('path');
-// core module in node.js
-var mime = require('mime');
+const http = require('http');
+// var fs = require('fs');
+// var path = require('path');
+// const { log } = require('console');
 
+// explain whats mime
+// mime is a module that provides ability to derive a MIME type based on filename extension
 
-
-// cache object
-var cache = {};
-
-
-
-
-// 404 error
-function send404(response) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found.');
-    response.end();
-}
-// send file
-function sendFile(response, filePath, fileContents) {
-    response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
-    response.end(fileContents);
-}
-
-
-
-// serve static files
-function serveStatic(response, cache, absPath) {
-    // check if file is cached in memory
-    if (cache[absPath]) {
-        // serve file from memory
-        sendFile(response, absPath, cache[absPath]);
-    } else {
-        // check if file exists
-        fs.exists(absPath, function(exists) {
-            // file exists
-            if (exists) {
-                // read file from disk
-                fs.readFile(absPath, function(err, data) {
-                    // error
-                    if (err) {
-                        send404(response);
-                    } else {
-                        // cache file in memory
-                        cache[absPath] = data;
-                        // serve file from disk
-                        sendFile(response, absPath, data);
-                    }
-                });
-            } else {
-                send404(response);
-            }
-        });
-    }
-}
-
+// Status codes and their meaning
+// 200 ok
+// 201 created
+// 202 accepted
 // 301 redirect
 // 304 not modified
 // 400 bad request
@@ -68,26 +19,25 @@ function serveStatic(response, cache, absPath) {
 // 500 internal server error
 // 503 service unavailable
 // 504 gateway timeout
-// 200 ok
-// 201 created
-// 202 accepted
-// 203 non-authoritative information
-// 204 no content
-// 205 reset content
-// 206 partial content
 
-// create http server
-var server = http.createServer(function(request, response) {
-    var filePath = false;
-    // default file
-    if (request.url == '/') {
-        filePath = 'public/index.html';
-    // translate url path to relative file path
-    } else {
-        filePath = 'public' + request.url;
-    }
-    // get absolute file path
-    var absPath = './' + filePath;
-    // serve static file
-    serveStatic(response, cache, absPath);
+const server = http.createServer((req, res) => {
+   console.log(`${req.method} request for ${req.url}`);
+   process.exit();
 });
+
+server.listen(3000);
+// // create http server
+// var server = http.createServer(function(request, response) {
+//     var filePath = false;
+//     // default file
+//     if (request.url == '/') {
+//         filePath = 'public/index.html';
+//     // translate url path to relative file path
+//     } else {
+//         filePath = 'public' + request.url;
+//     }
+//     // get absolute file path
+//     var absPath = './' + filePath;
+//     // serve static file
+//     serveStatic(response, cache, absPath);
+// });

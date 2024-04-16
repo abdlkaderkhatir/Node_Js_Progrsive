6760c833cc9d89622c6b2b4e6003b29abfa142fd
abdlkaderkhatir <a.khatir@esi-sba.dz>
2024-04-16 21:20:37 +0100

    using express router

--- server.js
+++ server.js
@@ -1,63 +1,14 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-// Status codes and their meaning
-// 200 ok
-// 201 created
-// 202 accepted
-// 301 redirect
-// 304 not modified
-// 400 bad request
-// 401 unauthorized
-// 403 forbidden
-// 404 not found
-// 500 internal server error
-// 503 service unavailable
-// 504 gateway timeout
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(`${req.method} request for ${req.url}`);
-  res.setHeader("Content-Type", "text/html");
-  if (req.url === "/") {
-    // add form here
-    res.write("<html>");
-    res.write("<head><title>first server</title></head>");
-    res.write("<body>");
-    res.write("<h1>Hello Khatir</h1>");
-    res.write("<form method='POST' action='/message'>");
-    res.write("<input type='text' name='username' />");
-    res.write("<input type='submit' value='submit' />");
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-  } else if (req.url === "/message" && req.method === "POST") {
-    // add form here
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedbody = Buffer.concat(body).toString();
-      const message = parsedbody.split("=")[1];
-      fs.writeFile("message.txt", message, () => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  // else if (req.url === "/about") {
-  res.write("<html>");
-  res.write("<head><title>first server</title></head>");
-  res.write("<body>");
-  res.write("<h1>Hello Khatir</h1>");
-  res.write("</body>");
-  res.write("</html>");
-  res.end();
-  // }
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  // process.exit();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3050);
+app.listen(3050);
